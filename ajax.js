20596464ddd3ff36464c766c87fa34884424b01e
Aholu Samuel <samuelaholu15@gmail.com>
2022-09-20 16:25:53 +0100

    Initial commit

---
+++ ajax.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  
+  document.getElementById('s_mail').value = '';
+//Register
+$("#reg").click(function () {
+  
+    var name  = $("#name").val();
+    var email = $("#email").val();
+    var company = $("#company").val();
+    var role = $("#role").val();
+    if (name == null || name == "") {
+      $("#msg").html("Kindly input your name");
+      
+    } else {
+    if (email == null || email == "") {
+        $("#msg").html("Kindly input your email");
+    }else{
+      if (company == null || company == "") {
+        $("#msg").html("Company can't be empty");
+    }else{
+      if (role == null || role == "") {
+        $("#msg").html("Role can't be empty");
+    }else{
+      $.ajax({
+        type: "post",
+        url: "submit.php",
+        data: {name: name, email:email, company: company, role: role},
+        success: function (data) {
+          $("#msg").html(data);
+        },
+      });
+    }
+    }}}
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('company').value = '';
+    document.getElementById('role').value = '';
+  });
+  
+
+//Subscribe
+$("#sub").click(function () {
+  
+    var s_mail = $("#s_mail").val();
+    if (mail == null || mail == "") {
+      $("#s_msg").html("Input Email");
+      
+    } else {
+    
+      $.ajax({
+        type: "post",
+        url: "submit.php",
+        data: {s_mail: s_mail},
+        success: function (data) {
+          $("#s_msg").html(data);
+        },
+      });
+    
+    }
+  });
+  document.getElementById('s_mail').value = '';
+});
---
+++ submit.php
@@ -0,0 +1,118 @@
+<?php
+$con = mysqli_connect("localhost","scribetr_reg","scriberpass5050","scribetr_reg");
+function query($query) {
+	global $con;
+
+	return mysqli_query($con, $query);
+}
+
+
+function insert() {
+	global $con;
+
+	return mysqli_insert_id($con);
+}
+
+
+function confirm($result) {
+	global $con;
+if(!$result) {
+
+	die("QUERY FAILED" . mysqli_error($con));
+}
+}
+
+
+function row_count($result) {
+
+	global $con;
+	
+		return mysqli_num_rows($result); 
+	}
+
+
+if ($con->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+  }
+
+
+function email_exist($email) {
+
+$sql = "SELECT * FROM register WHERE `email` = '$email'";
+$result = query($sql);
+
+if(row_count($result) == 1) {
+
+    return true;
+
+}else {
+
+    return false;
+} 
+}
+
+function s_email_exist($email) {
+
+	$sql = "SELECT * FROM subscribe WHERE `email` = '$email'";
+	$result = query($sql);
+	
+	if(row_count($result) == 1) {
+	
+		return true;
+	
+	}else {
+	
+		return false;
+	} 
+	}
+
+//Register
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['company']) && isset($_POST['role']) ) {
+	$name 		= $_POST['name'];
+	$email	= $_POST['email'];
+	$company = $_POST['company'];
+	
+        $role	= $_POST['role'];
+	if(email_exist($email)) {
+
+		echo "You already Registered!";
+		} else {
+		
+		
+
+        // Insert article into db 
+        $sqlt = "INSERT INTO register(`name`, `email`, `company`, `role`)";
+		$sqlt.= " VALUES('$name', '$email', '$company', '$role')";
+		$result = query($sqlt);
+		if ($result != 1) {
+			echo "Error! Please Consult Administrator.";
+		} else {
+			echo "Thank you for Registering! ";
+			echo 'Stay Glued to your Mailbox';
+		}
+}
+}
+
+//Subscribe
+if (isset($_POST['s_mail'])) {
+	$mail	= $_POST['s_mail'];
+
+	if(s_email_exist($mail)) {
+
+		echo "You already Subscribed";
+		} else {
+		
+		
+
+        // Insert article into db 
+        $sqlts = "INSERT INTO subscribe(`email`)";
+		$sqlts.= " VALUES('$mail')";
+		$result = query($sqlts);
+		if ($result != 1) {
+			echo "Error! Please Consult Administrator.";
+		} else {
+			echo "Thank you Subscribing! ";
+		}
+}
+}
+?>
